af5b1a5127e199a24526a4fef4e5f62c31fee944
sy <441502252@qq.com>
2022-06-25 22:38:34 +0800

    项目搭建-路由配置

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Link to="/login">登录</Link>
-        <Link to="/home">首页</Link>
-
+        {/* <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link> */}
         <Switch>
           <Route path="/home" component={Home}></Route>
           <Route path="/login" component={Login}></Route>
