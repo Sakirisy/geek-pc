b312586d05c574253f7eafc48e7f0e5e802c39c7
sy <441502252@qq.com>
2022-06-26 13:27:12 +0800

    登录功能-基本结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import { Card } from 'antd'
 import './index.css'
+import logo from '../../assets/logo.png'
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        <Card>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
+      <div className="login">
+        <Card className="login-container">
+          <img src={logo} alt="" className='login-logo' />
         </Card>
       </div>
     )
